d8fe8b191180c2d6fd8206552f452455d6643016
Dwi Rizki Aji <dwirizkiaji@gmail.com>
2023-09-19 09:38:12 +0700

    menambahkan alert js di form login

--- login.php
+++ login.php
@@ -13,7 +13,7 @@ if (isset($_POST["login"])) {
   $username = $_POST["username"];
   $password = $_POST["password"];
 
-  $result = mysqli_query($conn, "SELECT * FROM users WHERE username = '$username'");
+  $result = mysqli_query($conn, "SELECT * FROM user WHERE username = '$username'");
 
   //cek username
   if (mysqli_num_rows($result) === 1) {
